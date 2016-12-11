245405451ab370ff8e64d170e8229b0bb6b23377
zhiwenxuan <1045875816@qq.com>
2016-12-12 03:01:53 +0800

    feat(goodStudentDao.js) 增加评优帖的dao文件

---
+++ src/dao/goodStudentDao.js
@@ -0,0 +1,103 @@
+const mysql = require('mysql');
+const $conf = require('../config');
+const $sql = require('./goodStudentSql');
+
+let pool = mysql.createPool($conf.mysql);
+
+let jsonWrite = function (res, ret) {
+	if(typeof ret === 'undefined') {
+		res.status (500).json ({
+			code:'error',
+			msg: '操作失败'
+		});
+	} else {
+		res.status (200).json (ret);
+	}
+};
+
+module.exports = {
+	add: function (req, res, next) {
+		pool.getConnection(function(err, connection) {
+			// 获取前台页面传过来的参数
+			let param = req.query || req.params;
+			//let param = req.body || req.query || req.params;
+			// 建立连接，向表中插入值
+			//INSERT INTO good_student_posts (postId, studentId, goodStudent, title, howmuch, isDeleted) VALUES (null,?,?,?,?,0)',
+			connection.query($sql.insert, [param.studentId, param.goodStudent, param.title, param.howmuch], function(err, result) {
+				if(result) {
+					console.log("insertPost:"+result);
+					_result = {
+						code: '0',
+						data:{
+							postId:result.insertId,
+						},
+						msg:'增加成功'
+					};    
+				}
+				// 以json形式，把操作结果返回给前台页面
+				jsonWrite(res, _result);
+				// 释放连接 
+				connection.release();
+			});
+		});
+	},
+    find: function(req, res, next){
+    	pool.getConnection(function(err, connection){
+    		let param = req.query || req.params;
+    		let startPage = param.page*20;
+    		let endPage = (param.page + 1)*20;
+            connection.query($sql.findAll, [param.classId, param.type, startPage, endPage], function(err, result){
+            	if(result) {
+            		console.log("findPost:"+result);
+            		if(result==""){
+            			_result = {
+            				code: '4002A',
+            				msg: '班级id不存在'
+            			}
+            		}else{
+            			_result = {
+            				code: '0',
+            				data: {
+            					posts:result
+            				},
+            				msg: '查找成功'
+            			}
+            		}
+            		jsonWrite(res, _result);
+            		connection.release();
+            	}
+            });
+    	});
+    },
+    delete: function(req, res, next){
+    	pool.getConnection(function(err, connection){
+    		let param = req.query || req.params;
+    		//let param = req.body || req.query || req.params;
+            connection.query($sql.delete, [param.postId], function(err, result){
+            	if(result.affectedRows) {
+            		console.log("deletePost:"+result.affectedRows);
+            		_result = {
+            			code: '0',
+            			msg: '删除成功'
+            		}
+            		jsonWrite(res, _result);
+            		connection.release();
+            	}else{
+                    console.log("deletePost:"+result.affectedRows);
+                    _result = {
+                        code: '4003A',
+                        msg: '帖子id不存在'
+                    }
+                    jsonWrite(res, _result);
+                    connection.release();
+                }
+
+            });
+    	});
+    }
+};
+
+
+//http://localhost:3000/api/goodStudent?studentId=1&goodStudent=1&howmuch=1500&title=学业一等奖
+//http://localhost:3000/api/goodStudent?postId=1
+//http://localhost:3000/api/goodStudent?classId=1&page=0&type=postId
