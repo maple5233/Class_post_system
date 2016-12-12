5071c0ae89524dadef23b6483fb6bc054cb946f8
zhiwenxuan <1045875816@qq.com>
2016-12-12 23:58:51 +0800

    feat(meetingAPI): 增加同意名单

--- src/dao/meetingDao.js
+++ src/dao/meetingDao.js
@@ -9,7 +9,9 @@ module.exports = {
 	add: function (req, res, next) {
 		pool.getConnection(function(err, connection) {
 			// 获取前台页面传过来的参数
-			let param = req.query || req.params;
+			let param = req.body || req.query || req.params;
+            //let param = req.query || req.params;
+            let list = param.list, _result;
 			// 建立连接，向表中插入值
 			// (postId, studentId, time, place, numPeople, numShow, title, isDeleted) VALUES (null,?,?,?,?,?,?,0)',
 			connection.query($sql.insert, [param.studentId, param.time, param.place, param.numPeople,param.numShow,param.title], function(err, result) {
@@ -21,8 +23,16 @@ module.exports = {
 							postId:result.insertId,
 						},
 						msg:'增加成功'
-					};    
-				}
+					};
+                    //插入同意信息关系表
+                    list.forEach(item =>{
+                        //INSERT INTO agree_infos (agreeInfoId, studentId, postId, agreeInfo) VALUES(null, ?,?,?)
+                        connection.query($sql.insertAgree, [item.studentId, result.insertId, item.agreeInfo], function(err, ret){
+                            console.dir(ret);
+                        });                        
+                    });
+                    
+                }
 				// 以json形式，把操作结果返回给前台页面
 				jsonWrite(res, _result);
 				// 释放连接 
@@ -33,10 +43,9 @@ module.exports = {
     find: function(req, res, next){
     	pool.getConnection(function(err, connection){
     		console.log("find");
-    		//let param = req.query || req.params ;
-    		let param = req.body;
-    		let startPage = param.page*20;
-    		let endPage = (param.page + 1)*20;
+    		let param = req.query || req.params ;
+    		let startPage = param.page*20, endPage = (param.page + 1)*20;
+    		let _result, agreeId, disagreeId;
             connection.query($sql.findAll, [param.classId, param.type, startPage, endPage], function(err, result){
             	if(result) {
             		console.log("findPost:"+result);
@@ -46,19 +55,39 @@ module.exports = {
             				msg: '班级id不存在'
             			}
             		}else{
-            			_result = {
-            				code: '0',
-            				data: {
-            					posts:result
-            				},
-            				msg: '查找成功'
-            			}
-            		}
-            		jsonWrite(res, _result);
-            		connection.release();
-            	}
+                        let postRes = result;
+                        for(let i=0; i<postRes.length; i++){
+                            connection.query($sql.findAgree, [postRes[i].postId], function(err, ret){
+                               if(ret == undefined || ret=="" ){
+                                   agreeId=[];
+                               } else {
+                                   agreeId = ret
+                               }
+                              postRes[i].agreeId = agreeId;
+                              if(i==postRes.length-1){
+                                _result = {
+                                    code: '0',
+                                    data: {
+                                        posts:postRes
+                                    }
+                                }
+                                jsonWrite(res, _result);
+                                connection.release();
+                              }
+                            });
+                        }
+                    }
+
+                }else{
+                    _result = {
+                        code: '3002B',
+                        msg: '未知错误'
+                    }
+                    jsonWrite(res, _result);
+                    connection.release();
+                }
             });
-    	});
+        });
     },
     delete: function(req, res, next){
     	pool.getConnection(function(err, connection){
@@ -84,10 +113,11 @@ module.exports = {
                 }
 
             });
-    	});
+        });
     }
 };
 
 
 //http://localhost:3000/api/meeting?studentId=1&time=2016-12-01 00:00:01& place=南区&numPeople=40&numShow=30&title=班会
 //http://localhost:3000/api/meeting?postId=1
+//http://localhost:3000/api/meeting?classId=1&page=0&type=postId
--- src/dao/meetingSql.js
+++ src/dao/meetingSql.js
@@ -1,7 +1,10 @@
 let meetingSql = {
 	insert:'INSERT INTO class_meeting_posts (postId, studentId, time, place, numPeople, numShow, title, isDeleted) VALUES (null,?,?,?,?,?,?,0)',
 	findAll:'select * from class_meeting_posts where studentId in (select studentId from students where classId =?) and isDeleted=0 order by ? limit ?,?',
-	delete:'update class_meeting_posts SET isDeleted = 1 where postId = ?'
+	findDisgree:'select studentId from agree_infos where studentId in (select studentId from students where classId =?) and agreeInfo=0',
+    findAgree:'select studentId from agree_infos where postId=? and agreeInfo=1',
+	delete:'update class_meeting_posts SET isDeleted = 1 where postId = ?',
+	insertAgree: 'INSERT INTO agree_infos (agreeInfoId, studentId, postId, agreeInfo) VALUES(null, ?,?,?)'
 };
 
 module.exports = meetingSql;
