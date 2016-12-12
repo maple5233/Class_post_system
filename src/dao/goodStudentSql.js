60e7472b0a9a798af71d1586e4ade3a2e3d39d10
zhiwenxuan <1045875816@qq.com>
2016-12-12 17:44:46 +0800

    fix(goodStudentSql): 更正sql查询语句

--- src/dao/goodStudentSql.js
+++ src/dao/goodStudentSql.js
@@ -1,6 +1,6 @@
 let goodStudentSql = {
 	insert:'INSERT INTO good_student_posts (postId, studentId, goodStudent, title, howmuch, isDeleted) VALUES (null,?,?,?,?,0)',
-	findAll:'select * from good_student_posts where studentId = (select studentId from students where classId =?) order by ? limit ?,?',
+	findAll:'select * from good_student_posts where studentId in (select studentId from students where classId =?) and isDeleted=0 order by ? limit ?,?',
 	delete:'update good_student_posts SET isDeleted = 1 where postId = ?'
 };
 
