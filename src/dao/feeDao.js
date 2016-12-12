5cba48d540d99dbe6b6886596cf8f35d654b104a
zhiwenxuan <1045875816@qq.com>
2016-12-13 02:15:52 +0800

    feat(feeAPI): 增加插入查询党费关系表

--- src/dao/feeDao.js
+++ src/dao/feeDao.js
@@ -10,6 +10,7 @@ module.exports = {
 		pool.getConnection(function(err, connection) {
 			// 获取前台页面传过来的参数
             let param = req.body || req.query || req.params;
+             let list = param.list, _result;
 			// 建立连接，向表中插入值
 			//INSERT INTO party_fee_posts (postId, studentId, time, howmuch, isDeleted) VALUES (null,?,?,?,0)
 			connection.query($sql.insert, [param.studentId, param.time, param.howmuch], function(err, result) {
@@ -21,7 +22,13 @@ module.exports = {
 							postId:result.insertId,
 						},
 						msg:'增加成功'
-					};    
+					}; 
+                   list.forEach(item =>{
+                        //INSERT INTO party_fee_posts (feeInfoId, studentId, postId, feeInfo) VALUES(null, ?,?,?)
+                        connection.query($sql.insertFeeInfo, [item.studentId, result.insertId, item.feeInfo], function(err, ret){
+                            console.dir(ret);
+                        });                        
+                    });    
 				}
 				// 以json形式，把操作结果返回给前台页面
 				jsonWrite(res, _result);
@@ -35,6 +42,7 @@ module.exports = {
             let param = req.query || req.params;
     		let startPage = param.page*20;
     		let endPage = (param.page + 1)*20;
+            let _result, payId
             connection.query($sql.findAll, [param.classId, param.type, startPage, endPage], function(err, result){
             	if(result) {
             		console.log("findPost:"+result);
@@ -44,17 +52,37 @@ module.exports = {
             				msg: '班级id不存在'
             			}
             		}else{
-            			_result = {
-            				code: '0',
-            				data: {
-            					posts:result
-            				},
-            				msg: '查找成功'
-            			}
+            			let postRes = result;
+                        for(let i=0; i<postRes.length; i++){
+                            connection.query($sql.findPayId, [postRes[i].postId], function(err, ret){
+                               if(ret == undefined || ret=="" ){
+                                   payId=[];
+                               } else {
+                                   payId = ret;
+
+                               }
+                              postRes[i].payId = payId;
+                              if(i==postRes.length-1){
+                                _result = {
+                                    code: '0',
+                                    data: {
+                                        posts:postRes
+                                    }
+                                }
+                                jsonWrite(res, _result);
+                                connection.release();
+                              }
+                            });
+                        }
             		}
-            		jsonWrite(res, _result);
-            		connection.release();
-            	}
+            	}else{
+                    _result = {
+                        code: '4002B',
+                        msg: '未知错误'
+                    }
+                    jsonWrite(res, _result);
+                    connection.release();
+                }
             });
     	});
     },
--- src/dao/feeSql.js
+++ src/dao/feeSql.js
@@ -1,7 +1,10 @@
 let feeSql = {
 	insert:'INSERT INTO party_fee_posts (postId, studentId, time, howmuch, isDeleted) VALUES (null,?,?,?,0)',
 	findAll:'select * from party_fee_posts where studentId in (select studentId from students where classId =?) and isDeleted=0 order by ? limit ?,?',
-	delete:'update party_fee_posts SET isDeleted = 1 where postId = ?'
+	delete:'update party_fee_posts SET isDeleted = 1 where postId = ?',
+	insertFeeInfo: 'INSERT INTO party_fee_posts (feeInfoId, studentId, postId, feeInfo) VALUES(null, ?,?,?)',
+	findPayId:'select studentId from fee_infos where postId=? and feeInfo=1',
+	findUnPayId:'select studentId from fee_infos where postId=? and feeInfo=1',
 };
 
 module.exports = feeSql;
