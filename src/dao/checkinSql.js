4ce1a601eb0eba3ea2100d5feeca8b4dc9615a0f
zhiwenxuan <1045875816@qq.com>
2016-12-12 17:45:26 +0800

    fix(checkinSql.js): 更正sql查询语句

--- src/dao/checkinSql.js
+++ src/dao/checkinSql.js
@@ -1,6 +1,6 @@
 let checkInSql = {
 	insert:'INSERT INTO check_in_posts (postId, studentId, badStudent, time, courseName, isDeleted) VALUES (null,?,?,?,?,0)',
-	findAll:'select * from check_in_posts where studentId = (select studentId from students where classId =?) order by ? limit ?,?',
+	findAll:'select * from check_in_posts where studentId in (select studentId from students where classId =?) and isDeleted=0 order by ? limit ?,?',
 	delete:'update check_in_posts SET isDeleted = 1 where postId = ?'
 };
 
