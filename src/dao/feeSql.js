ecc23b6eb1e8b7ef6d41161cbe901f3e447977d8
zhiwenxuan <1045875816@qq.com>
2016-12-12 17:45:05 +0800

    fix(feeSql.js): 更正sql查询语句

--- src/dao/feeSql.js
+++ src/dao/feeSql.js
@@ -1,6 +1,6 @@
 let feeSql = {
 	insert:'INSERT INTO party_fee_posts (postId, studentId, time, howmuch, isDeleted) VALUES (null,?,?,?,0)',
-	findAll:'select * from party_fee_posts where studentId = (select studentId from students where classId =?) order by ? limit ?,?',
+	findAll:'select * from party_fee_posts where studentId in (select studentId from students where classId =?) and isDeleted=0 order by ? limit ?,?',
 	delete:'update party_fee_posts SET isDeleted = 1 where postId = ?'
 };
 
