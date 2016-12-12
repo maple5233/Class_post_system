2336cddc01afdabc29a0b0ebcf758eb7b15a05f6
zhiwenxuan <1045875816@qq.com>
2016-12-12 17:03:37 +0800

    feat(answersDao): 新增回复贴dao文件

---
+++ src/dao/answersDao.js
@@ -0,0 +1,100 @@
+const mysql = require('mysql');
+const $conf = require('../config');
+const $sql = require('./answersSql');
+
+let pool = mysql.createPool($conf.mysql);
+
+let jsonWrite = function (res, ret) {
+	if(typeof ret === 'undefined') {
+		res.status (500).json ({
+			code:'error',
+			msg: '操作失败'
+		});
+	} else {
+		res.status (200).json (ret);
+	}
+};
+
+module.exports = {
+	add: function (req, res) {
+		pool.getConnection(function(err, connection) {
+			console.log('ddd');
+			// 获取前台页面传过来的参数
+			let param = req.query || req.params;
+			//let param = req.body || req.query || req.params;
+			// 建立连接，向表中插入值
+			//INSERT INTO answers (answerId, studentId, postKind, postId, content, time, isDeleted) VALUES (null,?,?,?,?,?,0)
+			connection.query($sql.insert, [param.studentId, param.postKind, param.postId, param.content, param.time], function(err, result) {
+				if(result) {
+					console.log("insertAnswer:"+result);
+					_result = {
+						code: '0',
+						data:{
+							answerId:result.insertId,
+						}
+					};    
+				}
+				// 以json形式，把操作结果返回给前台页面
+				jsonWrite(res, _result);
+				// 释放连接 
+				connection.release();
+			});
+		});
+	},
+    find: function(req, res){
+    	pool.getConnection(function(err, connection){
+    		let param = req.query || req.params;
+            connection.query($sql.findAll, [param.postKind, param.postId], function(err, result){
+            	if(result) {
+            		console.log("findPost:"+result);
+            		if(result==""){
+            			_result = {
+            				code: '7001A',
+            				msg: '帖子id不存在'
+            			}
+            		}else{
+            			_result = {
+            				code: '0',
+            				data: {
+            					answers:result
+            				}
+            			}
+            		}
+            		jsonWrite(res, _result);
+            		connection.release();
+            	}
+            });
+    	});
+    },
+    delete: function(req, res){
+    	pool.getConnection(function(err, connection){
+    		let param = req.query || req.params;
+    		//let param = req.body || req.query || req.params;
+            connection.query($sql.delete, [param.answerId], function(err, result){
+            	if(result.affectedRows) {
+            		console.log("deletePost:"+result.affectedRows);
+            		_result = {
+            			code: '0',
+            			msg: '删除成功'
+            		}
+            		jsonWrite(res, _result);
+            		connection.release();
+            	}else{
+                    console.log("deletePost:"+result.affectedRows);
+                    _result = {
+                        code: '7003A',
+                        msg: '帖子id不存在'
+                    }
+                    jsonWrite(res, _result);
+                    connection.release();
+                }
+
+            });
+    	});
+    }
+};
+
+
+//http://localhost:3000/api/answers?studentId=1&postKind=1&postId=1&content=有趣&time=2016-12-12
+//http://localhost:3000/api/answers?postId=1
+//http://localhost:3000/api/answers?classId=1&page=0&type=postId
