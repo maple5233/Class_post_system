c9378c4949d4a360c3afc2175d4f57295fbd9f18
Littlesqx <Littlesqx@gmail.com>
2016-12-13 01:36:09 +0800

    feat(student):增加认证模块

---
+++ src/client/student/src/service/authService.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import store from '../store.js'
+
+export default {
+    login(auth) {
+            return axios.post('/students/login', {
+                    studentName: auth.studentId,
+                    studentPass: auth.studentPass
+                })
+                .then(function(response) {
+                    console.log(response);
+                    if (response.body.code == '0') {
+                        // do something
+                        store.dispatch('saveAuth', response.body.data);
+                    } else {
+                        // code     msg
+                        // 1002A 学号不存在
+                        // 1002B   密码不正确
+                        // 1002C   未知错误
+                    }
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        },
+        register(auth) {
+            return axios.post('/students/register', {
+                    studentName: auth.studentName,
+                    studentPass: auth.studentPass,
+                    classId: auth.classId
+                })
+                .then(function(response) {
+                    console.log(response);
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        },
+        signout() {
+            return store.dispatch('clearAuth');
+        }
+}
