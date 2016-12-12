22002117f7e5bd79fceb14df1e626f14970a5bc4
zhiwenxuan <1045875816@qq.com>
2016-12-12 17:11:12 +0800

    feat(app.js): 增加回复贴路由接口

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ let meeting = require('./routes/meeting');
 let fee = require('./routes/fee');
 let goodStudent = require('./routes/goodStudent');
 let checkIn = require('./routes/checkin');
+let answers = require('./routes/answers')
 let student = require('./routes/student');
 let teacher = require('./routes/teacher');
 let login = require('./routes/login');
