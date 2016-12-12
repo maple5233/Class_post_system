1500fa6f872481271aab9e217bab6a0ae5dfd354
zhiwenxuan <1045875816@qq.com>
2016-12-12 17:43:29 +0800

    fix(meetingSql.js): 更正sql查询语句

--- src/dao/meetingSql.js
+++ src/dao/meetingSql.js
@@ -1,6 +1,6 @@
 let meetingSql = {
 	insert:'INSERT INTO class_meeting_posts (postId, studentId, time, place, numPeople, numShow, title, isDeleted) VALUES (null,?,?,?,?,?,?,0)',
-	findAll:'select * from class_meeting_posts where studentId = (select studentId from students where classId =?) order by ? limit ?,?',
+	findAll:'select * from class_meeting_posts where studentId in (select studentId from students where classId =?) and isDeleted=0 order by ? limit ?,?',
 	delete:'update class_meeting_posts SET isDeleted = 1 where postId = ?'
 };
 
