6e579ccfed7ee461f8a89f37844ba40722acb8e0
zhiwenxuan <1045875816@qq.com>
2016-12-12 17:44:14 +0800

    fix(studentSql.js): 更正sql查询语句

--- src/dao/studentSql.js
+++ src/dao/studentSql.js
@@ -1,5 +1,5 @@
 let studentSql = {
-	getList: 'select * from students where classId = (select classId from classes where teacherId =?) and isDeleted=0 order by ? limit ?,?',
+	getList: 'select * from students where classId in (select classId from classes where teacherId =?) and isDeleted=0 order by ? limit ?,?',
     getListByAdmin: 'select * from students  and isDeleted=0 order by ? limit ?,?',
     register: 'INSERT INTO students (studentId, studentName, studentPass, studentRank, classId) VALUES (null,?,?,0,?)',
     tryLogin: 'select * from students where studentId = ?',
