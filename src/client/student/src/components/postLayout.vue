e67856ca6a67b9261fbd11fefcbace81af01aebd
Littlesqx <Littlesqx@gmail.com>
2016-12-12 19:37:47 +0800

    update(student):修改评论框位置

--- src/client/student/src/components/postLayout.vue
+++ src/client/student/src/components/postLayout.vue
@@ -2,36 +2,37 @@
     <div>
         <el-row v-for="postItem in postItems" type="flex" id="content" justify="center">
             <el-col :xs="24" :sm="20" :md="16" :lg="14">
-                <el-popover placement="bottom-start" width="400">
-                    <el-card class="box-card" slot="reference">
-                        <div slot="header" class="clearfix">
-                            <span style="line-height: 36px; font-size: 1.3rem;">{{ postItem.title }}</span>
-                            <el-tag type="primary" class="type-tag">{{ postItem.type }}</el-tag>
-                            <div class="options">
-                                <el-tag type="gray">评论 <i class="el-icon-menu"></i></el-tag>
-                            </div>
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span style="line-height: 36px; font-size: 1.3rem;">{{ postItem.title }}</span>
+                        <el-tag type="primary" class="type-tag">{{ postItem.type }}</el-tag>
+                        <div class="options">
+                            <el-tag type="gray">删除 <i class="el-icon-delete"></i></el-tag>
+                            <el-popover placement="bottom-start" width="400">
+                                <el-tag type="gray" slot="reference">评论 <i class="el-icon-menu"></i></el-tag>
+                                <div v-loading="postItem.commentloading" element-loading-text="加载中" style="min-height: 120px">
+                                    <el-tabs v-for="commentItem in postItem.commentItems" type="border-card" class="comment-item">
+                                        <el-tab-pane :label="commentItem.author">{{ commentItem.content }}</el-tab-pane>
+                                    </el-tabs>
+                                    <el-pagination small layout="prev, pager, next" :total="50">
+                                    </el-pagination>
+                                    <el-input type="textarea" :autosize="{ minRows: 2}" placeholder="请输入内容" v-model="textarea" class="comment-area">
+                                    </el-input>
+                                    <el-button type="primary" size="small">提 交</el-button>
+                                    <el-button type="primary" size="small">重 置</el-button>
+                                </div>
+                            </el-popover>
                         </div>
-                        <div>
-                            <el-table :data="postItem.contentData" style="width: 100%">
-                                <el-table-column prop="howmuch" label="金额">
-                                </el-table-column>
-                                <el-table-column prop="time" label="截止日期">
-                                </el-table-column>
-                            </el-table>
-                        </div>
-                    </el-card>
-                    <div v-loading="postItem.commentloading" element-loading-text="加载中" style="min-height: 120px">
-                        <el-tabs v-for="commentItem in postItem.commentItems" type="border-card" class="comment-item">
-                            <el-tab-pane :label="commentItem.author">{{ commentItem.content }}</el-tab-pane>
-                        </el-tabs>
-                        <el-pagination small layout="prev, pager, next" :total="50">
-                        </el-pagination>
-                        <el-input type="textarea" :autosize="{ minRows: 2}" placeholder="请输入内容" v-model="textarea" class="comment-area">
-                        </el-input>
-                        <el-button type="primary" size="small">提 交</el-button>
-                        <el-button type="primary" size="small">重 置</el-button>
                     </div>
-                </el-popover>
+                    <div>
+                        <el-table :data="postItem.contentData" style="width: 100%">
+                            <el-table-column prop="howmuch" label="金额">
+                            </el-table-column>
+                            <el-table-column prop="time" label="截止日期">
+                            </el-table-column>
+                        </el-table>
+                    </div>
+                </el-card>
             </el-col>
         </el-row>
     </div>
